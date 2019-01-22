bb1551eac709a2bef0d1b56d6b696d0ffb4dc0bd
Usuario <Usuario@192.168.1.39>
2019-01-22 18:24:29 -0500

    Ubigeo, Venta (model)

---
+++ Cine/src/model/Ubigeo.java
@@ -0,0 +1,42 @@
+package model;
+
+public class Ubigeo {
+    private String idUbi;
+    private String dptoUbi;
+    private String provUbi;
+    private String distUbi;
+
+    public String getIdUbi() {
+        return idUbi;
+    }
+
+    public void setIdUbi(String idUbi) {
+        this.idUbi = idUbi;
+    }
+
+    public String getDptoUbi() {
+        return dptoUbi;
+    }
+
+    public void setDptoUbi(String dptoUbi) {
+        this.dptoUbi = dptoUbi;
+    }
+
+    public String getProvUbi() {
+        return provUbi;
+    }
+
+    public void setProvUbi(String provUbi) {
+        this.provUbi = provUbi;
+    }
+
+    public String getDistUbi() {
+        return distUbi;
+    }
+
+    public void setDistUbi(String distUbi) {
+        this.distUbi = distUbi;
+    }
+    
+    
+}
---
+++ Cine/src/model/Venta.java
@@ -0,0 +1,43 @@
+package model;
+
+public class Venta {
+    private int idVta;
+    private int idClie;
+    private int idPel;
+    private int totEnt;
+
+    public int getIdVta() {
+        return idVta;
+    }
+
+    public void setIdVta(int idVta) {
+        this.idVta = idVta;
+    }
+
+    public int getIdClie() {
+        return idClie;
+    }
+
+    public void setIdClie(int idClie) {
+        this.idClie = idClie;
+    }
+
+    public int getIdPel() {
+        return idPel;
+    }
+
+    public void setIdPel(int idPel) {
+        this.idPel = idPel;
+    }
+
+    public int getTotEnt() {
+        return totEnt;
+    }
+
+    public void setTotEnt(int totEnt) {
+        this.totEnt = totEnt;
+    }
+
+
+    
+}
