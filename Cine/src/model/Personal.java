463c81bac6797441cf300ab16e64b955177f7250
Luis Diego Rojas Ayllon <lrojas@vallegrande.edu.pe>
2019-01-24 20:18:02 -0500

    modelo "Personal"

--- Cine/src/model/Personal.java
+++ Cine/src/model/Personal.java
@@ -2,4 +2,88 @@ package model;
 
 public class Personal {
     
+    private int idPer;
+    private String nomPer;
+    private String apePer;
+    private String dniPer;
+    private String dirPer;
+    private String tipPer;
+    private String sexPer;
+    private String usuPer;
+    private String pwdPer;
+
+    public int getIdPer() {
+        return idPer;
+    }
+
+    public void setIdPer(int idPer) {
+        this.idPer = idPer;
+    }
+
+    public String getNomPer() {
+        return nomPer;
+    }
+
+    public void setNomPer(String nomPer) {
+        this.nomPer = nomPer;
+    }
+
+    public String getApePer() {
+        return apePer;
+    }
+
+    public void setApePer(String apePer) {
+        this.apePer = apePer;
+    }
+
+    public String getDniPer() {
+        return dniPer;
+    }
+
+    public void setDniPer(String dniPer) {
+        this.dniPer = dniPer;
+    }
+
+    public String getDirPer() {
+        return dirPer;
+    }
+
+    public void setDirPer(String dirPer) {
+        this.dirPer = dirPer;
+    }
+
+    public String getTipPer() {
+        return tipPer;
+    }
+
+    public void setTipPer(String tipPer) {
+        this.tipPer = tipPer;
+    }
+
+    public String getSexPer() {
+        return sexPer;
+    }
+
+    public void setSexPer(String sexPer) {
+        this.sexPer = sexPer;
+    }
+
+    public String getUsuPer() {
+        return usuPer;
+    }
+
+    public void setUsuPer(String usuPer) {
+        this.usuPer = usuPer;
+    }
+
+    public String getPwdPer() {
+        return pwdPer;
+    }
+
+    public void setPwdPer(String pwdPer) {
+        this.pwdPer = pwdPer;
+    }
+    
+    
+    
 }
